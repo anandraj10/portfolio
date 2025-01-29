61fb3f3cc622fee3dc5df98407c0f8aacd8a3d05
anandraj10 <anandraj36401@gmial.com>
2025-01-29 19:26:58 +0530

    error resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 
 import styles from "./App.module.css";
-import { Navbar } from "./components/Navbar/navbar";
-import {Hero} from "./components/Hero/Hero"
-import {About} from "./components/About/About"
+import { Navbar } from "./components/Navbar/Navbar";
+import { Hero } from "./components/Hero/Hero"
+import { About } from "./components/About/About"
 import { Experience } from "./components/Experience/Experience";
 import { Projects } from "./components/Projects/Projects";
 import { Contact } from "./components/Contact/Contact";
@@ -10,12 +10,12 @@ import { Contact } from "./components/Contact/Contact";
 function App() {
   return (
     <div className={styles.App}>
-      <Navbar/>
-      <Hero/>
-      <About/>
-      <Experience/>
-      <Projects/>
-      <Contact/>
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
     </div>
   );
 }
